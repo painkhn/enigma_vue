a11278004e0d6f7b3ae1bc2211a78cf2251f1010
Халтаев Никита Сергеевич <21073@iat.iat>
2024-09-26 16:14:50 +0800

    fixes

--- src/components/HeaderSearch.vue
+++ src/components/HeaderSearch.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="header__search justify-self-center w-full">
         <form action="" class="header__search-form flex items-center">
-            <input type="search" name="" id="" class="header__search-input w-full bg-white dark:bg-zinc-800 focus:bg-zinc-700 text-white outline-none px-4 h-8 rounded-md  transition-all border border-black dark:border-red-400 focus:shadow-md focus:shadow-black" placeholder="Поиск...">
+            <input type="search" name="" id="" class="header__search-input w-full focus:bg-white dark:bg-zinc-800 bg-gray-100 dark:focus:bg-zinc-700 text-white outline-none px-4 h-8 rounded-md  transition-all border border-black dark:border-red-400 focus:shadow-md dark:focus:shadow-black" placeholder="Поиск...">
             <input type="submit" value="S" class="header__search-submit w-8 h-8 bg-red-400 ml-2 rounded-md transition-all cursor-pointer border-2 border-black hover:shadow hover:shadow-black">
         </form>
     </div>
--- src/components/MainThemes.vue
+++ src/components/MainThemes.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="main__themes w-8/12 h-auto p-10 rounded-xl bg-white dark:bg-zinc-800">
+    <div class="main__themes w-8/12 h-auto p-10 rounded-xl bg-white dark:bg-zinc-800 shadow-xl">
         <h2 class="main__themes-title text-red-400 font-bold text-2xl mb-5">Название категории</h2>
         <ul class="main__themes-list flex flex-col gap-5 text-lg pl-4 dark:text-white">
             <li><router-link to="/theme" href="#!" class="transition-all hover:text-red-400">Название темы</router-link></li>
